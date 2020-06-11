2085205f7a6c1d4e5b1d600645db7953376f0f19
zhangxh <1965027608@qq.com>
2020-06-11 11:39:20 +0800

    修改错误

--- Week01/zeroMove.java
+++ Week01/zeroMove.java
@@ -52,14 +52,14 @@ class Solution {
 class Solution {
     public void moveZeroes(int[] nums) {
         //指针-----------------------------
-        int count = 0;
+        int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[j] = nums[i];
             } if (i != j) {
                 nums[i] = 0;
             }
-            count++;
+            j++;
         }
     }
 }
